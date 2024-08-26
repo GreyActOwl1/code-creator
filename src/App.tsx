4616c3093b021ddf34ab47c72ba900d48ad0a22c
GreyActOwl1 <106501057+GreyActOwl1@users.noreply.github.com>
2024-08-26 03:40:19 -0400

    Update Routes adds new routes for future features and a 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+// src/App.tsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ShowCreators from './pages/ShowCreators';
+import ViewCreator from './pages/ViewCreator';
+import EditCreator from './pages/EditCreator';
+import AddCreator from './pages/AddCreator';
+import NotFound from './pages/NotFound'; 
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/creators" element={<ShowCreators />} />
+        <Route path="/creators/add" element={<AddCreator />} />
+        {/* <Route path="/creators/:id" element={<ViewCreator />} /> */}
+        {/* <Route path="/creators/:id/edit" element={<EditCreator />} /> */}
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
